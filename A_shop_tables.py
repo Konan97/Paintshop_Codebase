4a97ef9f19385564ef2a7a2541362a6bbe29370b
Sun <YSUN98@volvocars.com>
2024-07-31 15:33:20 -0400

    modify A shop code

--- A_shop_tables.py
+++ A_shop_tables.py
@@ -13,7 +13,7 @@ import matplotlib
 
 stations = ['WE_GD', 'FIN_A', 'ARPR']
 dataset = dataset.loc[~dataset['Linking Workstation'].isin(stations)]
-options = ['RA258', 'RA292', 'RB007', 'RA245', 'RA402', 'RA410', 'RA401', 'RA411', 'B0540', 'RA251', 'B0102', 'RA09A', 'RA405', 'RA404', 'A0009', 'RA400', 'RA406', 'A0028', 'A0420', 'RAA0006', 'RA291', 'B0401']
+options = ['BUD1', 'RB258', 'B0185', 'B0680', 'RB292', 'RBS000', 'RB09A', 'B0102', 'BSEA1', 'BSEAL', 'RB251', 'RB400', 'RB401', 'RB402', 'RB007', 'RB245', 'RB100', 'RB444', 'RB405', 'RB446', 'RB0009', 'RB410', 'RB411', 'RB291', 'RB06', 'B0058', 'B0104', 'B0121', 'B0123', 'B0184', 'B021', 'B0421', 'B0570', 'B0670', 'RAA006', 'RB0002']
 dataset = dataset.loc[dataset['ATACQ Item Code'].isin(options)]
 
 dataset = dataset.dropna()
